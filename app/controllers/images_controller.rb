047fe62c1e773fee88003e6ed6eae6bb278afcb2
田畑友作 <kuppa0713@icloud.com>
2025-03-24 18:37:46 +0900

    最低限の画像アップロード機能を実装

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,29 @@
+class ImagesController < ApplicationController
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+  
+    # タイトルが送られてこなかった場合、自動的に設定
+    @image.title ||= "無題画像 - #{Time.current.strftime('%Y%m%d%H%M%S')}"
+  
+    if @image.save
+      redirect_to @image
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:file, :title)
+  end
+end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,35 @@
 class Image < ApplicationRecord
-    has_one_attached :file
-    has_one_attached :processed_file
+  has_one_attached :file
+  has_one_attached :processed_file
+
+  after_commit :process_image, on: [:create]
+
+  private
+
+  def process_image
+    return unless file.attached?
+
+    require "mini_magick"
+
+    # 一時ファイルを加工
+    downloaded = ActiveStorage::Blob.service.send(:path_for, file.blob.key)
+    image = MiniMagick::Image.open(downloaded)
+
+    # 加工内容（ここはお好みでどんどん追加できます）
+    image.combine_options do |c|
+      c.negate         # 色反転
+      c.contrast       # コントラスト強調
+      c.wave "5x80"    # 歪ませる
+    end
+
+    # 加工後のファイルを保存
+    temp_path = Rails.root.join("tmp", "processed_#{SecureRandom.hex(8)}.jpg")
+    image.write(temp_path)
+
+    # ActiveStorageに添付
+    processed_file.attach(io: File.open(temp_path), filename: "processed.jpg", content_type: "image/jpeg")
+
+    # 一時ファイル削除
+    File.delete(temp_path) if File.exist?(temp_path)
+  end
 end
